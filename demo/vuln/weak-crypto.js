991a1322b1eb95b013e1147a339bec1fed64cc91
Yered Cespedes <yered1@msn.com>
2025-08-11 07:26:09 +0000

    seed findings: fake secret + weak crypto

--- demo/vuln/weak-crypto.js
+++ demo/vuln/weak-crypto.js
@@ -1,3 +1,2 @@
 const crypto = require('crypto');
-const hash = crypto.createHash('md5').update('password').digest('hex'); // BAD
-console.log(hash);
+crypto.createHash('md5').update('password').digest('hex'); // BAD: weak hash
